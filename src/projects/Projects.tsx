fa27c166ef393522905e45bbd01af3793fbeece9
marceloProgramas <marcelomartinsfarinassojr@gmail.com>
2023-08-03 23:25:05 -0300

    calculadora

--- src/projects/Projects.tsx
+++ src/projects/Projects.tsx
@@ -1,18 +1,20 @@
 import * as S from './Projects'
 import { Link } from '../components/links'
 
-function Project({name,archive, alt, url, text, color}:{name:string,archive:string, alt: string, url:string, text:string, color:string}){
+function Project({name,archive, alt, url, width='30', text, color}:{name:string,archive:string, alt: string, url:string, width:string, text:string, color:string}){
     return(
         <S.wrapper id={name} color={color}>
             <Link
             src={archive}
             alt={alt}
             url={url}
-            width = '30'
+            width = {width}
             />
             <S.text
             color = {color == '#121212'? '#FEFEFE': '#121212'}
-            >{text}</S.text>
+            >
+                {text}
+            </S.text>
         </S.wrapper>
     )
 }
@@ -22,20 +24,13 @@ function Projects(){
         <>
             <Project
             name="git"
-            color='#121212'
-            archive={"github lt.svg"}
-            alt='link github'
-            url='https://github.com/marceloProgramas'
-            text = "Mussum Ipsum, cacilds vidis litro abertis.  Não sou faixa preta cumpadi, sou preto inteiris, inteiris. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Suco de cevadiss deixa as pessoas mais interessantis. Per aumento de cachacis, eu reclamis."
-            />
-            <Project
-            name="link"
             color='#FEFEFE'
-            archive={"linkedin.svg"}
-            alt='link github'
-            url='https://github.com/marceloProgramas'
-            text = "Mussum Ipsum, cacilds vidis litro abertis.  Não sou faixa preta cumpadi, sou preto inteiris, inteiris. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Suco de cevadiss deixa as pessoas mais interessantis. Per aumento de cachacis, eu reclamis."
-        />
+            archive={"calculadora.png"}
+            alt='investiment calculator'
+            url='https://investiment-calculator.vercel.app'
+            width = '45'
+            text = "Calculadora de investimento utilizável para grandes ou pequenos períodos e para investimentos de diferentes tamanhos."
+            />
         </>
     )
 }
